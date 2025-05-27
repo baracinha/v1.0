cc1517c8cd5c768b65b88a1bac880b6bd1e03df1
baracinha <francisco.baracinha@gmail.com>
2025-05-27 22:09:27 +0100

    ex4 python dario

---
+++ ex4.py
@@ -0,0 +1,79 @@
+'''Criar um programa em Python para gerenciar um catálogo de até 100 livros. O programa deve permitir que o usuário realize as seguintes operações: 
+Cadastrar livros: 
+O programa deve permitir que o usuário adicione novos livros ao catálogo, informando o título, o autor e o ano de publicação. 
+Procurar por livros: 
+O usuário deve poder procurar por título ou autor. O programa deve exibir as posições onde o livro foi encontrado e mostrar o conteúdo correspondente. 
+Excluir livros: 
+O usuário deve poder excluir um livro a partir da sua posição no catálogo. A posição será dada pelo índice do livro na lista. 
+Ordenar livros: 
+O programa deve permitir que o usuário ordene os livros cadastrados por título ou autor. 
+Listar todos os livros cadastrados: 
+O programa deve listar todos os livros cadastrados, exibindo as informações de cada um (título, autor, ano). 
+Sair do programa: 
+O programa deve continuar em execução até que o usuário escolha explicitamente a opção de sair. 
+ 
+Menu do Programa: 
+O programa deve apresentar um menu com as seguintes opções: 
+Adicionar novo livro 
+Procurar por título ou autor 
+Excluir livro 
+Ordenar livros 
+Listar livros 
+Sair do Programa 
+ 
+Requisitos: 
+O programa deve permitir a execução contínua das operações, retornando sempre ao menu principal após cada ação. 
+As operações de busca, exclusão e ordenação devem ser realizadas com base nas listas de títulos e autores. 
+O programa deve garantir que não sejam cadastrados mais de 100 livros. 
+Adicionar a possibilidade de verificar se o livro já está cadastrado antes de permitir o cadastro. 
+ 
+8 pontos do teste estão destinados a: 
+ - (2) Criar Algoritmos de procura e de validação. 
+ - (2) Funções e reutilização das mesmas. 
+ - (1) Utilização de boas praticas e standards. 
+ - (1) Uso de Exceções. '''
+
+livros = []
+
+
+def adicionar_livro():
+    pass
+
+def excluir_livro():
+    pass
+
+def listar_livros():
+    pass
+
+def ordenar_livbros():
+    pass
+
+def sair():
+    pass
+
+def menu():
+    while True:
+        option = 0
+        print("1 - Adicionar novo livro")
+        print("2 - Procurar livro por titulo ou autor")
+        print("3 - Excluir livro")
+        print("4 - Ordenar livros")
+        print("5 - Listar livros")
+        print("6 - Sair do programa")
+
+        match option:
+            case '1':
+                pass
+            case '2':
+                pass
+            case '3':
+                pass
+            case '4':
+                pass
+            case '5':
+                pass
+            case '6':
+                break
+
+
+menu()
